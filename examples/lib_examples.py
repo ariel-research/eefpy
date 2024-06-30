a2847c72e916b9c3b2d27e86f9fd11b850429a60
Erel Segal-Halevi <erelsgl@gmail.com>
2024-06-30 13:58:34 +0300

    examples

--- examples/lib_examples.py
+++ examples/lib_examples.py
@@ -4,14 +4,21 @@ import logging
 if __name__ == "__main__":
     logger = logging.getLogger('eefpy')
     logger.addHandler(logging.StreamHandler())
+
+    print("*****Example from paper*****")
     num_agents = 3 
     num_types= 5
-
-    print("*****paper example*****")
-    agents = [[3,2,1,-1,3],[1,2,4,3,2],[1,3,1,3,-1]]
-    items = [4,10,4,2,1]
-    print(solve(num_agents=num_agents,num_types=num_types,agent_utils=agents,items=items, envy=EnvyNotion.NONE, obj=Objective.NONE))
+    agent_valuations = [[3,2,1,-1,3],[1,2,4,3,2],[1,3,1,3,-1]]
+    item_capacities = [4,10,4,2,1]
+    print(solve(num_agents=num_agents,num_types=num_types,agent_utils=agent_valuations,items=item_capacities, envy=EnvyNotion.NONE, obj=Objective.NONE))
 
     print("\n*****Example 2*****")
-    items = [111,442,317,294,312]
-    print(solve(num_agents=num_agents,num_types=num_types,agent_utils=agents,items=items, envy=EnvyNotion.NONE, obj=Objective.NONE))
+    item_capacities = [111,442,317,294,312]
+    print(solve(num_agents=num_agents,num_types=num_types,agent_utils=agent_valuations,items=item_capacities, envy=EnvyNotion.EF, obj=Objective.NONE))
+
+    print("\n*****Example 3*****")
+    num_agents = 4
+    num_types= 6
+    agent_valuations = [[100,60,60,60,70,60],[60,100,60,60,70,60],[60,60,100,60,60,70],[60,60,60,100,60,70]]
+    item_capacities = [2,2,2,2,2,2]
+    print(solve(num_agents=num_agents,num_types=num_types,agent_utils=agent_valuations,items=item_capacities, envy=EnvyNotion.EF, obj=Objective.NONE))
